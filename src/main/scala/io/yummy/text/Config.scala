4996209e91fc076abefd8ff37cc4fa5aa2ab2c3b
rfigueiredo <rafael.saraiva132@gmail.com>
2019-05-14 18:03:32 +0200

    style: Add proper logging for poorly loaded configuration files

--- src/main/scala/io/yummy/text/Config.scala
+++ src/main/scala/io/yummy/text/Config.scala
@@ -6,6 +6,8 @@ import cats.implicits._
 import io.chrisdavenport.log4cats.Logger
 import pureconfig.generic.auto._
 import pureconfig.module.catseffect.loadConfigF
+import scala.collection.JavaConverters._
+import scala.util.control.NonFatal
 
 case class Config(server: ServerConfig, digester: DigesterConfig)
 
@@ -16,10 +18,19 @@ object Config {
 
   private val basePath = "io.yummy.text"
 
+  private def dumpEnv[F[_]](implicit F: Sync[F], L: Logger[F]): PartialFunction[Throwable, F[Unit]] = {
+    case NonFatal(ex) ⇒
+      for {
+        e   ← F.delay(System.getenv().asScala.map(v ⇒ s"${v._1} = ${v._2}").mkString("\n", "\n", ""))
+        _   ← L.error(s"Loading configuration failed with the following environment variables: $e")
+        err ← F.raiseError[Unit](ex)
+      } yield err
+  }
+
   def apply[F[_]](implicit F: Sync[F], L: Logger[F]): F[Config] =
-    for {
+    (for {
       c ← loadConfigF[F, Config](basePath)
       _ ← L.info(s"Successfully loaded configuration to: $c")
-    } yield c
+    } yield c).onError(dumpEnv)
 
 }
