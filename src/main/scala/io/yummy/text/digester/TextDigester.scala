2d2a09e946157843b3b4ede14161d6d807d635b2
rfigueiredo <rafael.saraiva132@gmail.com>
2019-05-15 13:21:45 +0200

    refactor: separated word counter and occurrences into 2 different methods

--- src/main/scala/io/yummy/text/digester/TextDigester.scala
+++ src/main/scala/io/yummy/text/digester/TextDigester.scala
@@ -1,25 +1,32 @@
 package io.yummy.text.digester
 
 import cats.effect.Sync
+import cats.implicits._
 import io.yummy.text.model.{DigestedText, IngestedText}
 
 case class TextDigester[F[_]]()(implicit F: Sync[F]) {
 
-  def digest(text: IngestedText): F[DigestedText] = F.delay {
-    val words = text.value
+  private def retrieveWords(text: IngestedText): F[Seq[String]] = F.delay {
+    text.value
       .filter(r => r.isLetter || r.isSpaceChar)
       .toLowerCase
       .split(" ")
       .filterNot(_.isEmpty)
+  }
 
-    val occurrences = words
+  private def countOccurrences(words: Seq[String]): F[Map[String, Int]] = F.delay {
+    words
       .groupBy(identity)
       .map {
         case (key, value) =>
           (key, value.length)
       }
-
-    DigestedText(words.length, occurrences)
   }
+
+  def digest(text: IngestedText): F[DigestedText] =
+    for {
+      words       <- retrieveWords(text)
+      occurrences <- countOccurrences(words)
+    } yield DigestedText(words.length, occurrences)
 
 }
